6d8ceb931311416484bae8790dff0cb95f74bbf3
Vahid Hedayati <badvad@gmail.com>
2016-05-11 22:30:40 +0100

    .

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '3.0.1')
-        classNode.putNodeMetaData('projectName', 'mailinglist')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
